07992e0cd3748a9c800692d647b00643590d99c7
Cali Knowlden <calbearnc@gmail.com>
2024-01-25 19:06:41 -0600

    added bg image and styling to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Header.css';
+import image from '../../../public/images/grocery.jpg';
 
 function Header() {
-    return (
-        <header className="banner-header">
-            <h1>My Shopping List</h1>
-        </header>
-    );
+  return (
+    // <header className="banner-header">
+    <header
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        width: '100vw',
+        height: '10vh',
+        alignItems: 'center',
+      }}
+    >
+      <h1 className="banner-header">My Shopping List</h1>
+    </header>
+  );
 }
 
 export default Header;
