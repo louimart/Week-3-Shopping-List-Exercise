747af67eb8cc80c0c67dad657014a7d9fdf20f97
Louis Martin <louimart@gmail.com>
2024-01-27 04:53:36 -0600

    RESET button fuctioning, resetting all buy status to False in database and rendering BUY on client side

--- database.sql
+++ database.sql
@@ -6,17 +6,23 @@ CREATE TABLE "shopping_list" (
 	"id" SERIAL PRIMARY KEY,
 	"Name" VARCHAR (80) NOT NULL,
 	"Quantity" NUMERIC,
-	"Unit" 	VARCHAR (20) NOT NULL
+	"Unit" 	VARCHAR (20) NOT NULL,
+  "Buy" BOOLEAN DEFAULT FALSE
 );
 
-INSERT INTO "shopping_list" 
-	("Name", "Quantity", "Unit") 
-VALUES 
+INSERT INTO "shopping_list"
+	("Name", "Quantity", "Unit")
+VALUES
 	('Bread', '1.52', 'loaf'),
 	('Milk', '1', 'gal'),
 	('Eggs', '1', 'carton');
-	
 
 SELECT * FROM "shopping_list";
 
-DROP TABLE "shopping_list"
+DROP TABLE "shopping_list";
+
+DELETE FROM "shopping_list" *;
+
+UPDATE "shopping_list" SET "Buy" = NOT "Buy" WHERE "id" = 76;
+
+UPDATE "shopping_list" SET "Buy" = FALSE;
--- src/groceryApi/grocery.api.js
+++ src/groceryApi/grocery.api.js
@@ -5,18 +5,27 @@ export const fetchGrocery = () => {
   return axios.get('/api/groceries');
 };
 
+// POST - adds new grocery item
 export const postGrocery = (groceryData) => {
   return axios.post('/api/groceries', groceryData);
 };
 
+// DELETE - removes clicked grocery item
 export const deleteGrocery = (groceryId) => {
   return axios.delete(`/api/groceries/${groceryId}`);
 };
 
+// DELETE - removes all grocery items
 export const globalDeleteGrocery = (groceryList) => {
   return axios.delete('/api/global', groceryList);
 };
 
+// PUT - Toggles Buy status True/False (PURCHASED/BUY)
 export const updateBuyStatus = (groceryId) => {
   return axios.put(`/api/groceries/${groceryId}`);
+};
+
+// PUT - Reset all Buy status to False/Buy
+export const resetGlobalBuyStatus = (groceryList) => {
+  return axios.put('/api/global', groceryList);
 };
